e718da01b7324854e8d3a665e9141182e1a9a12d
Carlos Meza <Carlos@droots.org>
2014-06-25 12:37:23 -0700

    fix debian/run.sh

--- debian/run.sh
+++ debian/run.sh
@@ -1,9 +1,3 @@
-FROM debian:latest
-MAINTAINER Carlos Meza <carlos@droots.org>
-ENV DEBIAN_FRONTEND noninteractive
-RUN apt-get update
-RUN apt-get upgrade -y
-RUN apt-get install -y vim
-ADD run.sh /run.sh
-RUN chmod +x /run.sh
-CMD ["/run.sh"]
+#!/bin/bash
+source /etc/apache2/envvars
+exec apache2 -D FOREGROUND
